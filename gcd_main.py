1582cce7377d7029e378adb64e556ce9e907c742
Magnum Enforcer <rouldnoughi3@gatech.edu>
2022-02-24 17:44:31 -0500

    add loss function

--- gcd_main.py
+++ gcd_main.py
@@ -14,8 +14,10 @@ from torchvision import datasets, transforms
 from utils import progress_bar
 from loss.spc import SupervisedContrastiveLoss
 from data.cifarloader import CIFAR10Loader, CIFAR100Loader
+from data.rotationloader import DataLoader, GenericDataset
 import utils
 import vision_transformer as vits
+from vision_transformer import DINOHead
 from tqdm import tqdm
 
 def parse_args():
@@ -28,8 +30,8 @@ def parse_args():
     )
     parser.add_argument("--batch_size", default=128, type=int, help="On the contrastive step this will be multiplied by two.")
     parser.add_argument("--temperature", default=0.07, type=float, help="Constant for loss no thorough")
-    parser.add_argument("--n_epochs", default=300, type=int)
-    parser.add_argument("--lr_contrastive", default=1e-1, type=float)
+    parser.add_argument("--n_epochs", default=25, type=int)
+    parser.add_argument("--lr", default=1e-1, type=float)
     parser.add_argument("--weight_decay", type=float, default=1e-4, help="Weight decay for SGD")
     parser.add_argument("--momentum", default=0.9, type=float, help="Momentum for SGD")
     parser.add_argument("--num_workers", default=4, type=int, help="number of workers for Dataloader")
@@ -42,10 +44,6 @@ def parse_args():
     return args
 
 def dino_vitb16_nohead(pretrained=True, **kwargs):
-    """
-    ViT-Base/16x16 pre-trained with DINO.
-    Achieves 76.1% top-1 accuracy on ImageNet with k-NN classification.
-    """
     model = vits.__dict__["vit_base"](patch_size=16, num_classes=0, **kwargs)
     if pretrained:
         state_dict = torch.hub.load_state_dict_from_url(
@@ -55,7 +53,7 @@ def dino_vitb16_nohead(pretrained=True, **kwargs):
         model.load_state_dict(state_dict, strict=True)
     return model
 
-def train_contrastive(model, train_loader, eval_loader, criterion, optimizer, writer, args):
+def supervised_train(model, train_loader, eval_loader, criterion, optimizer, writer, args):
     """
     :param model: torch.nn.Module Model
     :param train_loader: torch.utils.data.DataLoader
@@ -73,15 +71,15 @@ def train_contrastive(model, train_loader, eval_loader, criterion, optimizer, wr
         print("Epoch [%d/%d]" % (epoch + 1, args.n_epochs))
         loss_record = utils.AverageMeter()
         train_loss = 0
-        exp_lr_scheduler.step()
         for batch_idx, (x, label, idx) in enumerate(tqdm(train_loader)):
             x, label = x.to(args.device), label.to(args.device)
-            output1 = model(x)
-            loss= criterion(output1, label)
-            loss_record.update(loss.item(), x.size(0))
             optimizer.zero_grad()
+            output = model(x)
+            loss= criterion(output, label)
+            loss_record.update(loss.item(), x.size(0))
             loss.backward()
             optimizer.step()
+
             train_loss += loss.item()
             writer.add_scalar(
                 "Loss train | Supervised Contrastive",
@@ -94,18 +92,21 @@ def train_contrastive(model, train_loader, eval_loader, criterion, optimizer, wr
                 "Loss: %.3f " % loss_record.avg,
             )
 
+        if epoch >= 5:
+            exp_lr_scheduler.step()
+
         # Only check every 10 epochs otherwise you will always save
-        if epoch % 20 == 0:
+        if epoch % 10 == 0:
             if loss_record.avg < best_loss:
-                print("Saving..")
-                state = {
-                    "net": model.state_dict(),
-                    "avg_loss": loss_record.avg,
-                    "epoch": epoch,
-                }
-                if not os.path.isdir("checkpoint"):
-                    os.mkdir("checkpoint")
-                torch.save(state, "./checkpoint/ckpt_contrastive.pth")
+                # print("Saving..")
+                # state = {
+                #     "net": model.state_dict(),
+                #     "avg_loss": loss_record.avg,
+                #     "epoch": epoch,
+                # }
+                # if not os.path.isdir("checkpoint"):
+                #     os.mkdir("checkpoint")
+                # torch.save(state, "./checkpoint/ckpt_contrastive.pth")
                 best_loss = loss_record.avg
 
         test(epoch, model, eval_loader, criterion, writer, args)
@@ -124,20 +125,21 @@ def test(epoch, model, test_loader, criterion, writer, args):
     """
 
     model.eval()
+
     test_loss = 0
     correct = 0
     total = 0
 
     with torch.no_grad():
-        for batch_idx, (inputs, targets, idx) in enumerate(test_loader):
-            inputs, targets = inputs.to(args.device), targets.to(args.device)
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
+        for batch_idx, (x, label, idx) in enumerate(tqdm(test_loader)):
+            x, label = x.to(args.device), label.to(args.device)
+            output = model(x)
+            loss = criterion(output, label)
 
             test_loss += loss.item()
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
+            _, predicted = output.max(1)
+            total += label.size(0)
+            correct += predicted.eq(label).sum().item()
 
             progress_bar(
                 batch_idx,
@@ -156,15 +158,15 @@ def test(epoch, model, test_loader, criterion, writer, args):
     writer.add_scalar("Accuracy validation | Cross Entropy", acc, epoch)
 
     if acc > args.best_acc:
-        print("Saving..")
-        state = {
-            "net": model.state_dict(),
-            "acc": acc,
-            "epoch": epoch,
-        }
-        if not os.path.isdir("checkpoint"):
-            os.mkdir("checkpoint")
-        torch.save(state, "./checkpoint/ckpt_cross_entropy.pth")
+        # print("Saving..")
+        # state = {
+        #     "net": model.state_dict(),
+        #     "acc": acc,
+        #     "epoch": epoch,
+        # }
+        # if not os.path.isdir("checkpoint"):
+        #     os.mkdir("checkpoint")
+        # torch.save(state, "./checkpoint/ckpt_cross_entropy.pth")
         args.best_acc = acc
 
 
@@ -173,43 +175,61 @@ def main():
     device = "cuda:2" if torch.cuda.is_available() else "cpu"
     args.device = device
 
+    #upervised
     if args.dataset == "cifar100":
-        labeled_train_loader = CIFAR100Loader(root=args.dataset_root, batch_size=args.batch_size, split='train', aug='once', shuffle=True, target_list = range(args.num_labeled_classes))
-        labeled_eval_loader = CIFAR100Loader(root=args.dataset_root, batch_size=args.batch_size, split='test', aug=None, shuffle=False, target_list = range(args.num_labeled_classes))
+        s_labeled_train_loader = CIFAR100Loader(root=args.dataset_root, batch_size=args.batch_size, split='train', aug='once', shuffle=True, target_list = range(args.num_labeled_classes))
+        s_labeled_eval_loader = CIFAR100Loader(root=args.dataset_root, batch_size=args.batch_size, split='test', aug=None, shuffle=False, target_list = range(args.num_labeled_classes))
         num_classes =  args.num_labeled_classes + args.num_unlabeled_classes
     else:
-        labeled_train_loader = CIFAR10Loader(root=args.dataset_root, batch_size=args.batch_size, split='train', aug='once', shuffle=True, target_list = range(args.num_labeled_classes))
-        labeled_eval_loader = CIFAR10Loader(root=args.dataset_root, batch_size=args.batch_size, split='test', aug=None, shuffle=False, target_list = range(args.num_labeled_classes))
+        s_labeled_train_loader = CIFAR10Loader(root=args.dataset_root, batch_size=args.batch_size, split='train', aug='once', shuffle=True, target_list = range(args.num_labeled_classes))
+        s_labeled_eval_loader = CIFAR10Loader(root=args.dataset_root, batch_size=args.batch_size, split='test', aug=None, shuffle=False, target_list = range(args.num_labeled_classes))
         num_classes = args.num_labeled_classes + args.num_unlabeled_classes
 
-    #load dino
+    #unsupervised
+    u_dataset_train = GenericDataset(dataset_name=args.dataset, split='train', dataset_root=args.dataset_root)
+    u_dataset_test = GenericDataset(dataset_name=args.dataset, split='test', dataset_root=args.dataset_root)
+    u_dloader_train = DataLoader(dataset=u_dataset_train, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=True)
+    u_dloader_test = DataLoader(dataset=u_dataset_test, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=False)
+
+
+    # load dino (no classfication head)
     model = dino_vitb16_nohead()
-    model = model.to(device)
-
-    cudnn.benchmark = True
-
-    if not os.path.isdir("logs"):
-        os.makedirs("logs")
-
-    writer = SummaryWriter("logs")
+    # freeze model
+    # for param in model.parameters():
+    #     param.requires_grad = False
+    # #set classifier
+    # model.head = DINOHead(in_dim=32, out_dim=num_classes)
+    # for param in model.head.parameters():
+    #     param.requires_grad = True
     
-    optimizer = optim.SGD(
-        model.parameters(),
-        lr=args.lr_contrastive,
-        momentum=args.momentum,
-        weight_decay=args.weight_decay,
-    )
+    
+    print(model)
+    # model = model.to(device)
 
-    args.best_acc = 0.0
-    criterion = SupervisedContrastiveLoss(temperature=args.temperature)
-    criterion.to(args.device)
-    train_contrastive(model, labeled_train_loader, labeled_eval_loader, criterion, optimizer, writer, args)
+    # cudnn.benchmark = True
 
-    # Load checkpoint.
-    print("==> Resuming from checkpoint..")
-    assert os.path.isdir("checkpoint"), "Error: no checkpoint directory found!"
-    checkpoint = torch.load("./checkpoint/ckpt_contrastive.pth")
-    model.load_state_dict(checkpoint["net"])
+    # if not os.path.isdir("logs"):
+    #     os.makedirs("logs")
+
+    # writer = SummaryWriter("logs")
+    
+    # optimizer = optim.SGD(
+    #     model.parameters(),
+    #     lr=args.lr,
+    #     momentum=args.momentum,
+    #     weight_decay=args.weight_decay,
+    # )
+
+    # args.best_acc = 0.0
+    # criterion = SupervisedContrastiveLoss(temperature=args.temperature)
+    # criterion.to(args.device)
+    # supervised_train(model, s_labeled_train_loader, s_labeled_eval_loader, criterion, optimizer, writer, args)
+
+    # # Load checkpoint.
+    # print("==> Resuming from checkpoint..")
+    # assert os.path.isdir("checkpoint"), "Error: no checkpoint directory found!"
+    # checkpoint = torch.load("./checkpoint/ckpt_contrastive.pth")
+    # model.load_state_dict(checkpoint["net"])
 
 
 if __name__ == "__main__":
--- loss/spc.py
+++ loss/spc.py
@@ -1,38 +1,61 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+from pytorch_metric_learning import losses
 
 from math import log
+
 class SupervisedContrastiveLoss(nn.Module):
     def __init__(self, temperature=0.07):
-        """
-        Implementation of the loss described in the paper Supervised Contrastive Learning :
-        https://arxiv.org/abs/2004.11362
-        :param temperature: int
-        """
         super(SupervisedContrastiveLoss, self).__init__()
         self.temperature = temperature
 
-    def forward(self, projections, targets):
-        """
-        :param projections: torch.Tensor, shape [batch_size, projection_dim]
-        :param targets: torch.Tensor, shape [batch_size]
-        :return: torch.Tensor, scalar
-        """
-        device = torch.device("cuda:2") if projections.is_cuda else torch.device("cpu")
-
-        dot_product_tempered = torch.mm(projections, projections.T) / self.temperature
-        # Minus max for numerical stability with exponential. Same done in cross entropy. Epsilon added to avoid log(0)
-        exp_dot_tempered = (
-            torch.exp(dot_product_tempered - torch.max(dot_product_tempered, dim=1, keepdim=True)[0]) + 1e-5
+    def forward(self, feature_vectors, labels):
+        # Normalize feature vectors
+        feature_vectors_normalized = F.normalize(feature_vectors, p=2, dim=1)
+        # Compute logits
+        logits = torch.div(
+            torch.matmul(
+                feature_vectors_normalized, torch.transpose(feature_vectors_normalized, 0, 1)
+            ),
+            self.temperature,
         )
+        return losses.NTXentLoss(temperature=0.07)(logits, torch.squeeze(labels))
 
-        mask_similar_class = (targets.unsqueeze(1).repeat(1, targets.shape[0]) == targets).to(device)
-        mask_anchor_out = (1 - torch.eye(exp_dot_tempered.shape[0])).to(device)
-        mask_combined = mask_similar_class * mask_anchor_out
-        cardinality_per_samples = torch.sum(mask_combined, dim=1)
 
-        log_prob = -torch.log(exp_dot_tempered / (torch.sum(exp_dot_tempered * mask_anchor_out, dim=1, keepdim=True)))
-        supervised_contrastive_loss_per_sample = torch.sum(log_prob * mask_combined, dim=1) / cardinality_per_samples
-        supervised_contrastive_loss = torch.mean(supervised_contrastive_loss_per_sample)
+# class SupervisedContrastiveLoss(nn.Module):
+#     def __init__(self, temperature=0.07):
+#         """
+#         Implementation of the loss described in the paper Supervised Contrastive Learning :
+#         https://arxiv.org/abs/2004.11362
+#         :param temperature: int
+#         """
+#         super(SupervisedContrastiveLoss, self).__init__()
+#         self.temperature = temperature
 
-        return supervised_contrastive_loss
+#     def forward(self, projections, targets):
+#         """
+#         :param projections: torch.Tensor, shape [batch_size, projection_dim]
+#         :param targets: torch.Tensor, shape [batch_size]
+#         :return: torch.Tensor, scalar
+#         """
+        
+#         device = torch.device("cuda:2") if projections.is_cuda else torch.device("cpu")
+
+#         dot_product_tempered = torch.mm(projections, projections.T) / self.temperature
+#         # Minus max for numerical stability with exponential. Same done in cross entropy. Epsilon added to avoid log(0)
+#         exp_dot_tempered = (
+#             torch.exp(dot_product_tempered - torch.max(dot_product_tempered, dim=1, keepdim=True)[0]) + 1e-5
+#         )
+
+#         mask_similar_class = (targets.unsqueeze(1).repeat(1, targets.shape[0]) == targets).to(device)
+#         mask_anchor_out = (1 - torch.eye(exp_dot_tempered.shape[0])).to(device)
+#         mask_combined = mask_similar_class * mask_anchor_out
+#         cardinality_per_samples = torch.sum(mask_combined, dim=1)
+
+#         log_prob = -torch.log(exp_dot_tempered / (torch.sum(exp_dot_tempered * mask_anchor_out, dim=1, keepdim=True)))
+#         supervised_contrastive_loss_per_sample = torch.sum(log_prob * mask_combined, dim=1) / cardinality_per_samples
+#         supervised_contrastive_loss = torch.mean(supervised_contrastive_loss_per_sample)
+
+#         return supervised_contrastive_loss
